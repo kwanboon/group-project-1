22efc523c8a7d4e73d953270d5616ba1e9c0aa39
chiamtj <chiamtj@gmail.com>
2021-12-07 23:20:39 +0800

    Fixture list not refreshing

--- src/components/EplTeams.js
+++ src/components/EplTeams.js
@@ -1,14 +1,15 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import API from "../components/API";
 
 function EplTeams(props) {
 
-    const defaultState = {
-        id:33,
-        fixture:0,
-    };
+    // const defaultState = {
+    //     id:33,
+    //     fixture:0,
+    // };
 
-    const [selectedTeam, setNewTeam] = useState(defaultState);
+    const [selectedTeam, setNewTeam] = useState(33);
+    const [selectedFixture, setNewFixture] = useState(0);
 
     const eplTeamArray = [{
         id: 33,
@@ -33,47 +34,65 @@ function EplTeams(props) {
     ];
 
     let returndata = [];
+    let  listItems = [];
+    
+    useEffect(()=>{
+        console.log("Team Changed");
+    },[selectedTeam]);
 
     async function fetchTeamFixtures() {
-            const response = await API.get("/fixtures", {params:{season:2021, team:selectedTeam.id, last:5}});
-            console.log ("Getting Fixtures data: ", selectedTeam.id);
+            const response = await API.get("/fixtures", {params:{season:2021, team:selectedTeam, last:5}});
+            console.log ("Getting Fixtures data: ", selectedTeam);
 
             if (response.status  === 200) {
                 returndata = response.data.response;
             }
-            console.log("Team Fixture Data:", returndata);
-    
+            // console.log("Team Fixture Data:", returndata);
+
+            console.log("List of Fixtures",
+                returndata.map((t) => 
+                    t.fixture.id
+                )
+            )
+
+            listItems = returndata.map((t) => {
+                return (
+                    <option value={t.fixture.id}>{t.fixture.date}</option>
+                )}
+            );
+
+            console.log(listItems)
+        
         }
 
     function filterTeams(e) {
         e.preventDefault();
-        setNewTeam(defaultState);
-        selectedTeam.id = e.target.value
-        console.log("NewTeamID:", selectedTeam.id);
+        setNewTeam(e.target.value);
+        // selectedTeam.id = e.target.value
+        console.log("NewTeamID:", selectedTeam);
 
         fetchTeamFixtures();
     }
 
     function filterFixtures(e) {
         e.preventDefault();
-        selectedTeam.fixture = e.target.value
-        console.log("Selected Fixture: ", selectedTeam.fixture);
+        setNewFixture(e.target.value);
+        console.log("Selected Fixture: ", selectedFixture);
     }
+
 
     return(
             <form>
                 <div>
                 <select onChange={filterTeams}>
                     {eplTeamArray.map((t) => 
-                    <option value={t.id}>{t.name}</option>
+                    <option key={t.id} value={t.id}>{t.name}</option>
                     )}
                 </select>
                 </div>
                 <div>
                 <select onChange={filterFixtures}>
-                    {returndata.map((t) => 
-                    <option value={t.id}>{t.date}</option>
-                    )}
+                    {listItems}
                 </select>
                 </div>
             </form>
